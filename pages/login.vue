60dfcfc4b0cd6a495b7cbf9ef66b114356f98065
giangnkt2k <54323844+giangnkt2k@users.noreply.github.com>
2022-11-10 11:13:38 +0700

    update

--- pages/login.vue
+++ pages/login.vue
@@ -1,43 +1,51 @@
 <template>
   <div class="login">
     <div class="logo">
-      <van-image
-        width="100"
-        height="100"
-        src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
-      />
+      <img width="100" height="100" src="../assets/img/logo.jpg">
     </div>
     <div class="login__form">
       <h1>{{ $t("login") }}</h1>
       <van-form @submit="onSubmit">
         <van-cell-group inset>
-          <van-field v-model="username" name="Username" :placeholder="$t('login_username')"
-          :rules="[{ required: true, message: $t('login_validate_require_username') }]"
-          :label="$t('login_username')" />
+          <van-field
+            v-model="username"
+            name="Username"
+            :placeholder="$t('login_username')"
+            :rules="[
+              {
+                required: true,
+                message: $t('login_validate_require_username'),
+              },
+            ]"
+            :label="$t('login_username')"
+          />
           <van-field
             v-model="password"
             type="password"
             name="Password"
-            :label="$t('login_passwoerd')"
-            :placeholder="$t('login_passwoerd')"
-            :rules="[{ required: true, message: $t('login_validate_password') }]"
+            :label="$t('login_password')"
+            :placeholder="$t('login_password')"
+            :rules="[
+              { required: true, message: $t('login_validate_password') },
+            ]"
           />
         </van-cell-group>
         <div style="margin: 16px">
           <van-button round block type="primary" native-type="submit">
-            {{ $t('login') }}
+            {{ $t("login") }}
           </van-button>
         </div>
       </van-form>
-      <van-grid :border="false" :column-num="2">
-        <van-grid-item text="" />
-        <van-grid-item to="/register" :text="$t('login_register')" />
-      </van-grid>
+      <van-row style="padding: 10px 16px;">
+        <van-col span="12"></van-col>
+        <van-col span="12" style="text-align:right;">
+          <a href="#/register" class="">{{ $t('login_register') }}</a>
+        </van-col>
+      </van-row>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-
 const username = ref("");
 const password = ref("");
 
