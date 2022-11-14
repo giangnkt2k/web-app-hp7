c6623b447cefc1009b73c9dfb7278bd05ab92381
giangnkt2k <54323844+giangnkt2k@users.noreply.github.com>
2022-11-14 19:31:56 +0700

    profle-kyc

---
+++ pages/profile/index.vue
@@ -0,0 +1,75 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <div class="mx-5 mt-20">
+      <div class="flex">
+        <van-image
+          round
+          width="5rem"
+          height="5rem"
+          src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+        />
+        <div class="ml-5">
+          <h1> hahaa </h1>
+          <h3>Real name</h3>
+        </div>
+      </div>
+
+      <div class="mt-10 p-3 bg-light-50 rounded-md">
+        <div class="flex">
+          <p>Total assets</p>
+          <van-icon name="eye-o" color="black" size="18" class="ml-3" />
+        </div>
+        <div class="my-3 text-2xl font-bold">
+          593243.23
+        </div>
+        <div class="flex justify-between">
+          <div>
+            Availiable: 30000000
+          </div>
+          <div>
+            Freeze: 231241414
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-5 p-3 bg-light-50 rounded-md flex justify-center">
+        <div class="border-r p-2 text-center">
+          <div class="text-sm">
+            Total market capitalization
+          </div>
+          <div class="text-xl font-bold">
+            95514.00
+          </div>
+        </div>
+        <div class="p-2 text-center">
+          <div class="text-sm">
+            Profit and loss of open positions
+          </div>
+          <div class="text-xl font-bold">
+            44791.00
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-5 mb-15 bg-light-50 rounded-md">
+        <van-cell-group>
+          <van-cell icon="records" title="Transaction history" is-link to="" />
+          <van-cell icon="thumb-circle-o" title="My Subscription" is-link to="" />
+          <van-cell icon="orders-o" title="Breakdown of funds" is-link to="" />
+          <van-cell icon="peer-pay" title="Silver certificate transfer out" is-link to="" />
+          <van-cell icon="description" title="Deposit list" is-link to="" />
+          <van-cell icon="cash-on-deliver" title="List of withdrawals" is-link to="" />
+          <van-cell icon="contact" title="Real-name authentication" is-link to="profile/kyc" />
+          <van-cell icon="browsing-history-o" title="Login password" is-link to="" />
+          <van-cell icon="browsing-history" title="Withdrawal password" is-link to="" />
+        </van-cell-group>
+      </div>
+
+      <TheBottomNavigation />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/profile/kyc.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <van-nav-bar
+      title="Real-name authentication"
+      left-text="Back"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+
+    <div class="mx-5 mt-20   bg-light-50 rounded-md">
+      <van-cell-group inset>
+        <van-field v-model="realName" label="Real name" />
+        <van-field v-model="tel" type="tel" label="Phone" />
+        <div class="text-sm py-3 px-4">
+          <div class="kyc__label pb-2">
+            ID card (front)
+          </div>
+          <van-uploader v-model="idCardFont" :disabled="idCardFont.length > 0" max-count="1" image-fit>
+            <template #preview-cover="{ file }">
+              <div class="preview-cover van-ellipsis">
+                {{ file.name }}
+              </div>
+            </template>
+          </van-uploader>
+        </div>
+        <div class="text-sm py-3 px-4">
+          <div class="kyc__label pb-2">
+            ID card (back)
+          </div>
+          <van-uploader v-model="idCardBack" :disabled="idCardBack.length > 0" max-count="1" image-fit>
+            <template #preview-cover="{ file }">
+              <div class="preview-cover van-ellipsis">
+                {{ file.name }}
+              </div>
+            </template>
+          </van-uploader>
+        </div>
+      </van-cell-group>
+    </div>
+    <div class="mx-5 my-5">
+      <van-button type="primary" block>
+        Submmit
+      </van-button>
+    </div>
+    <TheBottomNavigation />
+  </div>
+</template>
+<script lang="ts" setup>
+const onClickLeft = () => history.back()
+const realName = ref('')
+const tel = ref('')
+const idCardFont = ref([])
+const idCardBack = ref([])
+// const
+</script>
+<style lang="scss" scoped>
+  .preview-cover {
+    position: absolute;
+    bottom: 0;
+    box-sizing: border-box;
+    width: 100%;
+    padding: 4px;
+    color: #fff;
+    font-size: 12px;
+    text-align: center;
+    background: rgba(0, 0, 0, 0.3);
+  }
+  .kyc__label {
+    color:#646566;
+  }
+</style>
