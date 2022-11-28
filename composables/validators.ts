095b742d2010eae7a1e1cc83ea544ba541340a31
harison <nguyenhaison@Nguyens-MacBook-Pro-Aspire.local>
2022-11-28 23:14:03 +0700

    Remove old i18n

--- composables/validators.ts
+++ composables/validators.ts
@@ -1,14 +1,14 @@
 import * as validators from '@vuelidate/validators'
 
 export const useValidators = () => {
-  const { t } = useI18n()
+  const { $t } = useNuxtApp()
 
   const { createI18nMessage } = validators
 
   const messagePath = (data: { $validator: string; $propertyPath: string }) =>
   `validations.${data.$validator}.${data.$propertyPath}`
 
-  const withI18n = createI18nMessage({ t, messagePath })
+  const withI18n = createI18nMessage({ t: $t, messagePath })
 
   const required = withI18n(validators.required)
   const sameAs = withI18n(validators.sameAs, { withArguments: true })
